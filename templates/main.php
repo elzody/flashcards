11e6af8a02b6e31da07a7bbfc9d1c2f861707009
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-07-30 17:20:08 -0400

    Inject main css into template
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- templates/main.php
+++ templates/main.php
@@ -2,6 +2,7 @@
 
 use OCA\Flashcards\AppInfo\Application;
 
+\OCP\Util::addStyle(Application::APP_NAME, Application::APP_NAME . '-main');
 \OCP\Util::addScript(Application::APP_NAME, Application::APP_NAME . '-main');
 
 ?>
