49f7a422d3c930cc651d7c3afabd72b6df2a0a4e
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-11 22:14:07 -0400

    feat: add prettierignore
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- templates/main.php
+++ templates/main.php
@@ -4,7 +4,6 @@ use OCA\Flashcards\AppInfo\Application;
 
 \OCP\Util::addStyle(Application::APP_NAME, Application::APP_NAME . '-main');
 \OCP\Util::addScript(Application::APP_NAME, Application::APP_NAME . '-main');
-
 ?>
 
 <div id="content"></div>
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { createAppConfig } from '@nextcloud/vite-config'
 import path from 'path'
 
 export default createAppConfig({
-  main: path.resolve(__dirname, 'src', 'main.js'),
+	main: path.resolve(__dirname, 'src', 'main.js'),
 })
