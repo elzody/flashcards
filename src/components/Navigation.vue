be1ef806d2802ff1bd0863f82d8c90c3e97c5cac
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-13 23:26:21 -0400

    feat: add navigation component
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

---
+++ src/components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+	<NcAppNavigation>
+		<template #list>
+			<NcAppNavigationItem name="first item" />
+		</template>
+	</NcAppNavigation>
+</template>
+
+<script setup>
+import NcAppNavigation from '@nextcloud/vue/dist/Components/NcAppNavigation.js'
+import NcAppNavigationItem from '@nextcloud/vue/dist/Components/NcAppNavigationItem.js'
+</script>
