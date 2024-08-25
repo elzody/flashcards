4c2302aa05733d43d23c2896413ca6c42831fffe
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-25 15:47:06 -0400

    fix: remove unnecessary code from vite config
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,6 @@
 import path from 'path'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
 import { createAppConfig } from '@nextcloud/vite-config'
 
-/*// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-  	outDir: 'js/',
-  	assetsDir: '',
-  	cssCodeSplit: false,
-  	sourceMap: true,
-  	emptyOutDir: true,
-  	rollupOptions: {
-  		input: {
-  			main: path.resolve(__dirname, 'src', 'main.js'),
-  		},
-  	},
-  },
-})*/
-
-const overrides = defineConfig({
-	plugins: [vue()],
+export default createAppConfig({
+	main: path.resolve(__dirname, 'src', 'main.ts'),
 })
-
-export default createAppConfig(
-	{
-		main: path.resolve(__dirname, 'src', 'main.ts'),
-	} /*, {
-	config: overrides,
-}*/,
-)
