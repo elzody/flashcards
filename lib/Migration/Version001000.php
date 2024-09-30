fd389413d2a02e5cb852f80c2e6e34dd9890541a
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-09-30 19:27:49 -0400

    feat: begin work on db migration
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

---
+++ lib/Migration/Version001000.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OCA\Flashcards\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version001000 extends SimpleMigrationStep
+{
+	public function changeSchema(
+		IOutput $output,
+		Closure $schemaClosure,
+		array $options
+	): ?ISchemaWrapper {
+		$schema = $schemaClosure($options);
+
+		$this->createDecksTable($schema);
+
+		return $schema;
+	}
+
+	private function createDecksTable(mixed $schema): void {
+		$table = 'flashcards_decks';
+
+		if (!$schema->hasTable($table)) {
+			$table = $schema->createTable($table);
+
+			$table->addColumn('id', Types::INTEGER, [
+				'autoincrement' => true,
+				'notnull' => true,
+			]);
+
+			$table->addColumn('name', Types::STRING, [
+				'autoincrement' => false,
+				'notnull' => true,
+			]);
+
+			$table->addColumn('emoji', Types::STRING, [
+				'notnull' => false,
+			]);
+
+			$table->setPrimaryKey(['id']);
+		}
+	}
+}
