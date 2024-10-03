f65ee9bf1ff11b80561eb138ead79d4113ee598e
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-10-02 20:13:18 -0400

    feat: add owner to deck entity
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- lib/Db/Deck.php
+++ lib/Db/Deck.php
@@ -7,6 +7,7 @@ use OCP\AppFramework\Db\Entity;
 
 class Deck extends Entity
 {
+	protected ?string $owner = null;
 	protected ?string $name = null;
 	protected ?string $emoji = null;
 	protected ?DateTime $createdAt = null;
@@ -14,6 +15,7 @@ class Deck extends Entity
 	public function __construct()
 	{
 		$this->addType('id', 'integer');
+		$this->addType('owner', 'string');
 		$this->addType('name', 'string');
 		$this->addType('emoji', 'string');
 		$this->addType('createdAt', 'datetime');
