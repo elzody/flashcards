432e55caaa6d3e42a0c8beb08edf856a66e66963
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-06-11 22:53:27 -0400

    chore: update JS dependencies and fix app layout
    - Add engines (node ^24.0.0, npm ^11.3.0) and browserslist config
    - Update all JS dependencies to latest within existing ranges
      (vue 3.5.38, @nextcloud/vue 9.8.2, vite 7.3.5, eslint 10.4.1, etc.)
    - Wrap app in NcContent/NcAppContent as required by @nextcloud/vue 9.8+
      to fix invisible navigation bar on NC 34+
    
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-	<FlashcardsNav />
+	<NcContent app-name="flashcards">
+		<FlashcardsNav />
+		<NcAppContent>
+			<!-- Main content will go here -->
+		</NcAppContent>
+	</NcContent>
 </template>
 
 <script>
+import NcContent from '@nextcloud/vue/components/NcContent'
+import NcAppContent from '@nextcloud/vue/components/NcAppContent'
 import FlashcardsNav from './components/FlashcardsNav.vue'
 
 export default {
 	name: 'App',
 
 	components: {
+		NcContent,
+		NcAppContent,
 		FlashcardsNav,
 	},
 }
